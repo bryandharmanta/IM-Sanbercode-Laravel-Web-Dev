099afb6428f74ea3e0b6fdfbeb9acdb2c8cdc666
bryandharmanta <dharmantabryan@gmail.com>
2026-05-06 21:27:00 +0700

    belajar oop

---
+++ oop/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once("animal.php");
+require_once("frog.php");
+require_once("ape.php");
+
+$sheep = new Animal("shaun");
+
+echo "Name : " . $sheep->name . "<br>"; // "shaun"
+echo "legs : " . $sheep->legs . "<br>"; // 4
+echo "cold blooded : " . $sheep->cold_blooded . "<br>";// "no"
+echo "<br>";
+
+$kodok = new Frog("buduk");
+
+echo "Name : " . $kodok->name . "<br>";
+echo "legs : " . $kodok->legs . "<br>";
+echo "cold blooded : " . $kodok->cold_blooded . "<br>";
+echo "Jump : "; $kodok-> jump();
+echo "<br>";
+echo "<br>";
+
+$sungokong = new Ape("kera sakti");
+
+echo "Name : " . $sungokong->name . "<br>";
+echo "legs : " . $sungokong->legs . "<br>";
+echo "cold blooded : " . $sungokong->cold_blooded . "<br>";// "no"
+echo "Yell : "; $sungokong-> yell(); // "Auooo"
+
+?>
